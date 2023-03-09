dde0f8bc0f1a8040ac6bdfad92aad7ef9c0cdadb
hrithikcoder <108560837+hrithikcoder@users.noreply.github.com>
2023-03-09 09:39:01 +0530

    Update style-switcher.js

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -10,11 +10,12 @@ window.addEventListener("scroll",()=>{
 
     }
 })
+
 const alternateStyles=document.querySelectorAll(".alternate-style");
 function setActiveStyle(color)
 {
     alternateStyles.forEach((style)=> {
-        if(color === style.getAttribute("title")){
+        if(color ===style.getAttribute("title")){
             style.removeAttribute("disabled");
         }
         else{
